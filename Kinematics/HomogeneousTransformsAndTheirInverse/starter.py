16235cf48099340becbcdf1f018271cab121db35
George Fouche <fouliex@gmail.com>
2017-06-21 15:32:22 -0700

    Create folders

---
+++ Kinematics/HomogeneousTransformsAndTheirInverse/starter.py
@@ -0,0 +1,12 @@
+from sympy import symbols, cos, sin, pi , simplify ,sqrt , atan2
+from sympy.matrices import Matrix
+
+'''
+Problem statement:
+ Let P be a vector expressed in frame {B} with (x,y,z)
+ coordinates = (15.0,0.0,42.0)
+ Rotate P about the Y-axis by angle = 110 degrees
+ Then translate the vector 1 unit
+ in the X-axis and 30 units in
+
+'''
