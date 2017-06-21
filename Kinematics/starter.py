682ba38a187c0e8e311f85289d613141a5277b53
fouliex <fouliex@gmail.com>
2017-06-20 23:12:03 -0700

    add starter and roration_template file

---
+++ Kinematics/starter.py
@@ -0,0 +1,23 @@
+import Kinematics.rotation_template
+from  sympy import symbols, cos, sin, pi, sqrt
+from sympy.matrices import Matrix
+
+# Create a symbols for joint variables
+q1, q2 = symbols('q1:3')
+
+'''
+Create a symbolic matrix representing an extrinsic sequence of rotations about the Z and then Y axes.
+Let the rotation about the Y axis be described by q1 and the rotation about Z by q2
+'''
+# Replace R_y and R_z with the appropriate (symbolic) elementary rotation matrices
+R_y = Matrix([[ cos(q1),        0, sin(q1)],
+              [ 0,              1,       0],
+              [-sin(q1),        0, cos(q1)]])
+R_z = Matrix([[ cos(q2), -sin(q2),       0],
+              [ sin(q2),  cos(q2),       0],
+              [       0,        0,       1]])
+
+
+# Compute ZY_extrinsic
+ZY_extrinsic_sym = R_y * R_z
+ZY_extrinsic_num = ZY_extrinsic_sym.evalf(subs={q1: pi/4, q2: pi/3})
--- Kinematics/sympyExample.py
+++ Kinematics/sympyExample.py
@@ -2,34 +2,27 @@ from sympy import symbols, cos, sin, pi, simplify
 from sympy.matrices import Matrix
 import numpy as np
 
-##Create symbols for joint variables
+# Create symbols for joint variables
 q1, q2, q3, q4 = symbols('q1:5')  # slice doesn't include the end value
 # unrelated symbols can be defined like this:
 A, R, O, C = symbols('A R O C')
 
 # Conversion Factors
 rtd = 180. / np.pi  # radians to degrees
-dtr = np.pi/180 # degrees to radian
+dtr = np.pi / 180  # degrees to radian
 
 # Rotation matrices for  elementary rotation  on the X axes
-R_x = Matrix([[ 1,              0,        0],
-              [ 0,        cos(q1), -sin(q1)],
-              [ 0,        sin(q1),  cos(q1)]])
+R_x = Matrix([[1, 0, 0], [0, cos(q1), -sin(q1)], [0, sin(q1), cos(q1)]])
 
-#Rotation matrices for elementary rotation on the Y axes
-R_y = Matrix([[ cos(q2),        0,  sin(q2)],
-              [       0,        1,        0],
-              [-sin(q2),        0,  cos(q2)]])
+# Rotation matrices for elementary rotation on the Y axes
+R_y = Matrix([[cos(q2), 0, sin(q2)], [0, 1, 0], [-sin(q2), 0, cos(q2)]])
 
 # Rotation matrices for elementary rotation on the Z axes
-R_z = Matrix([[ cos(q3), -sin(q3),        0],
-              [ sin(q3),  cos(q3),        0],
-              [ 0,              0,        1]])
-
+R_z = Matrix([[cos(q3), -sin(q3), 0], [sin(q3), cos(q3), 0], [0, 0, 1]])
 
 print("Rotation about the X-axis by 45-degrees")
-print(R_x.evalf(subs={q1: 45*dtr}))
+print(R_x.evalf(subs={q1: 45 * dtr}))
 print("Rotation about the y-axis by 45-degrees")
-print(R_y.evalf(subs={q2: 45*dtr}))
+print(R_y.evalf(subs={q2: 45 * dtr}))
 print("Rotation about the Z-axis by 30-degrees")
-print(R_z.evalf(subs={q3: 30*dtr}))
+print(R_z.evalf(subs={q3: 30 * dtr}))
